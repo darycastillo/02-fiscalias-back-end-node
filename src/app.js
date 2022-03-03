ae0094b1cc2490572b6a8f2973c8cb9661cd905f
Dary Castillo <darycastillo27@gmail.com>
2022-03-03 07:00:44 -0600

    Se aniaden cors

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import pkg from "../package.json";
-/* Configuracion */
 import { routesSetup } from "./libs";
-/* Routes */
 import * as routes from "./routes";
-/* Instancia de express */
+
 const app = express();
-/* Setear package.json como variable */
 app.set("pkg", pkg);
-/* Visualizar las peticiones en consola */
+app.use(cors());
 app.use(morgan("dev"));
-/* Leer los request en formato json */
 app.use(express.json({ extended: true }));
-/* RUTAS */
-/* Ruta por default, informacion del backend */
+
 app.get("/", (req, res) => {
   res.json({
     name: app.get("pkg").name,
